5359d8d4a4ed71c321c1868c2734dc6834ed12b2
joshua <jyou12@berkeley.edu>
2021-10-23 14:40:12 -0700

    file uploading to backend

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const multer = require("multer");
+const upload = multer({dest: 'uploads/'});
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+app.post('/create-post', upload.single('media'), (request, response) => {
+    console.log(request.body);
+    console.log(request.file);
+    response.json({
+        'success': true
+    });
+});
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
