359a0e60b581e1a39fa1d8bb27bfecda19f60ef3
Tom Zheng <tomzheng@berkeley.edu>
2021-10-23 03:33:47 -0700

    temporary landing page

---
+++ frontend/src/components/Landing.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "../sass/main.scss";
+
+const Landing = () => {
+    return (
+        <div className="landing">
+            <div className="hero">
+                <div className="hero__nav">
+                    <div className="hero__nav-info">
+                        <a href="#">Tools</a>
+                        <a href="#">Plans</a>
+                        <a href="#">How it Works</a>
+                    </div>
+                    <div className="hero__nav-login">
+                        <a href="#">Register</a>
+                        <a href="#">Login</a>
+                    </div>
+                </div>
+                <div className="hero__headers">
+                    <h1>An All-In-One Tool to Manage Your Social Media</h1>
+                    <h4>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing
+                        elit. Harum, odit! Dolorum ab sequi eos perferendis odit
+                        suscipit, consectetur at laboriosam!
+                    </h4>
+                    <a className="hero__headers-btn">Get Started</a>
+                </div>
+            </div>
+            <div className="about"></div>
+        </div>
+    );
+};
+
+export default Landing;
